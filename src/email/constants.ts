e50cd8d950cdbb681bcf0110ba88f7c7419966eb
oussema sahbeni <oussemasahbeni300@gmail.com>
2026-05-31 13:53:57 +0100

    chore: migrate the project to a monorepo structure

--- src/email/constants.ts
+++
@@ -1,6 +0,0 @@
-export const primaryColor = "#2196F3";
-
-export const btnTextColor = "#ffffff";
-
-export const companyLogo =
-    "https://res.cloudinary.com/dyo9yeeck/image/upload/v1758804804/logo-text-on-dark_dcsfdr.svg";
